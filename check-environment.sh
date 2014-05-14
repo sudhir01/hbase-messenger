49eb20299c282b9bb43d78901e30c4662d05d993
Myer Nore <ptr.nore@gmail.com>
2014-05-14 09:27:03 -0400

    added check-environment.sh

--- check-environment.sh
+++ check-environment.sh
@@ -5,4 +5,7 @@ echo "HBase-Messenger has not been compiled yet.";
 command -v mvn >/dev/null 2>&1 || { echo "Maven is required to compile the plugin. Please install it correctly for your platform and run this script again." >&2; exit 1; } 
 echo "Maven has been found. Attempting to compile hbase messenger.";
 mvn package
+echo "If build was successful, then assuming you are runing hbase 0.96 and zookeeper, you you should be able to interact with hbase-messenger by using the hbase-messenger.sh shell script.
+else 
+echo "$JARFILE has been found. Assuming you are running hbase 0.96 and zookeeper, you should be able to interact with hbase-messenger by using the hbase-messenger.sh shell script."
 fi
